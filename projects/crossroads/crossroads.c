5ef3031710b91336f2027cfa24387e994f66a6af
seokjin1023 <babyy1023@naver.com>
2025-05-31 20:00:28 +0900

    0번 요구사항 parsing 완료

--- projects/crossroads/crossroads.c
+++ projects/crossroads/crossroads.c
@@ -17,108 +17,133 @@
 
 int crossroads_step;
 
-static void init_map_locks(struct lock ***map_locks) 
+static void init_map_locks(struct lock ***map_locks)
 {
-	int i, j;
-	struct lock **__map_locks;
+    int i, j;
+    struct lock **__map_locks;
 
-	__map_locks = *map_locks = malloc(sizeof (struct lock *) * 7);
-	for (i=0; i<7; i++) {
-		__map_locks[i] = malloc(sizeof (struct lock) * 7);
-		for (j=0; j<7; j++) {
-			lock_init(&__map_locks[i][j]);
-		}
-	}
+    __map_locks = *map_locks = malloc(sizeof(struct lock *) * 7);
+    for (i = 0; i < 7; i++)
+    {
+        __map_locks[i] = malloc(sizeof(struct lock) * 7);
+        for (j = 0; j < 7; j++)
+        {
+            lock_init(&__map_locks[i][j]);
+        }
+    }
 }
 
 static void release_map_locks(struct lock **map_locks)
 {
-	int i;
-	for (i=0; i<7; i++) {
-		free(map_locks[i]);
-	}
-	free(map_locks);
+    int i;
+    for (i = 0; i < 7; i++)
+    {
+        free(map_locks[i]);
+    }
+    free(map_locks);
 }
 
 static int is_finished(struct vehicle_info *vehicle_info, int thread_cnt)
 {
-	int i, res;
+    int i, res;
 
-	res = thread_cnt;
-	for (i=0; i<thread_cnt; i++) {
-		if (vehicle_info[i].state == VEHICLE_STATUS_FINISHED) {
-			res--;
-		}
-	}
+    res = thread_cnt;
+    for (i = 0; i < thread_cnt; i++)
+    {
+        if (vehicle_info[i].state == VEHICLE_STATUS_FINISHED)
+        {
+            res--;
+        }
+    }
 
-	/* return 0 when terminated */
-	return res;
+    /* return 0 when terminated */
+    return res;
+}
+
+void check_parse_vehicles(struct vehicle_info *vehicle_info, int thread_cnt)
+{
+    for (int i = 0; i < thread_cnt; i++)
+    {
+        printf(
+            "id = %c, start = %c, dest = %c, type = %d, arrival = %d, golden_time = %d, state = %d\n",
+            vehicle_info[i].id,               // 차량 ID
+            vehicle_info[i].start,            // 출발 노드
+            vehicle_info[i].dest,             // 도착 노드
+            (int)vehicle_info[i].type,        // VEHICL_TYPE_NORMAL(0) 또는 VEHICL_TYPE_AMBULANCE(1)
+            (int)vehicle_info[i].arrival,     // 정수형 도착 시간(없으면 0)
+            (int)vehicle_info[i].golden_time, // 정수형 골든 타임(없으면 0)
+            (int)vehicle_info[i].state        // VEHICLE_STATUS_READY(0), RUNNING(1), FINISHED(2)
+        );
+    }
 }
 
 void run_crossroads(char **argv)
 {
-	int i, thread_cnt;
-	struct lock **map_locks;
-	struct vehicle_info *vehicle_info;
-	struct blinker_info* blinkers;
+    int i, thread_cnt;
+    struct lock **map_locks;
+    struct vehicle_info *vehicle_info;
+    struct blinker_info *blinkers;
 
-	/* initialize unit step */
-	crossroads_step = 0;
+    /* initialize unit step */
+    crossroads_step = 0;
 
-	/* prepare crossroads map */
-	init_map_locks(&map_locks);
+    /* prepare crossroads map */
+    init_map_locks(&map_locks);
 
-	/* prepare vehicle data */
-	thread_cnt = 1;
-	for (i=0; (size_t) i<strlen(argv[1]); i++) {
-		if (argv[1][i] == ':') {
-			thread_cnt++;
-		}
-	}
-	printf("initializing %d vehicles...\n", thread_cnt);
-	vehicle_info = malloc(sizeof(struct vehicle_info) * thread_cnt);
-	parse_vehicles(vehicle_info, argv[1]);
+    /* prepare vehicle data */
+    thread_cnt = 1;
+    for (i = 0; (size_t)i < strlen(argv[1]); i++)
+    {
+        if (argv[1][i] == ':')
+        {
+            thread_cnt++;
+        }
+    }
+    printf("initializing %d vehicles...\n", thread_cnt);
+    vehicle_info = malloc(sizeof(struct vehicle_info) * thread_cnt);
+    parse_vehicles(vehicle_info, argv[1]);
+    check_parse_vehicles(vehicle_info, thread_cnt);
 
-	for (i=0; i<thread_cnt; i++) {
-		/* put map locks */
-		vehicle_info[i].map_locks = map_locks;
-	}
+    // 	for (i=0; i<thread_cnt; i++) {
+    // 		/* put map locks */
+    // 		vehicle_info[i].map_locks = map_locks;
+    // 	}
 
-	init_on_mainthread(thread_cnt);
+    // 	init_on_mainthread(thread_cnt);
 
-	blinkers = malloc(sizeof(struct blinker_info) * NUM_BLINKER);
-	init_blinker(blinkers, map_locks, vehicle_info);
+    // 	blinkers = malloc(sizeof(struct blinker_info) * NUM_BLINKER);
+    // 	init_blinker(blinkers, map_locks, vehicle_info);
 
-	/* prepare threads for each vehicle */ 
-	printf("initializing vehicle threads...\n");
-	for (i=0; i<thread_cnt; i++) {
-		char name[16];
-		snprintf(name, sizeof name, "thread %c", vehicle_info[i].id);
-		thread_create(name, PRI_DEFAULT, vehicle_loop, &vehicle_info[i]);
-	}
-	start_blinker();
+    // 	/* prepare threads for each vehicle */
+    // 	printf("initializing vehicle threads...\n");
+    // 	for (i=0; i<thread_cnt; i++) {
+    // 		char name[16];
+    // 		snprintf(name, sizeof name, "thread %c", vehicle_info[i].id);
+    // 		thread_create(name, PRI_DEFAULT, vehicle_loop, &vehicle_info[i]);
+    // 	}
+    // 	start_blinker();
 
-	printf("running project2 crossroads ...\n");
+    // 	printf("running project2 crossroads ...\n");
 
-#if 1
-	/* main loop */
-	do {
-		map_draw();
-		for (i=0; i<thread_cnt; i++) {
-			map_draw_vehicle(vehicle_info[i].id, 
-							vehicle_info[i].position.row,
-							vehicle_info[i].position.col);
-		}
-		/* sleep */
-		timer_msleep(1000);
-	} while (is_finished(vehicle_info, thread_cnt));
+    // #if 1
+    // 	/* main loop */
+    // 	do {
+    // 		map_draw();
+    // 		for (i=0; i<thread_cnt; i++) {
+    // 			map_draw_vehicle(vehicle_info[i].id,
+    // 							vehicle_info[i].position.row,
+    // 							vehicle_info[i].position.col);
+    // 		}
+    // 		/* sleep */
+    // 		timer_msleep(1000);
+    // 	} while (is_finished(vehicle_info, thread_cnt));
 
-	/* dealloc */
-	map_draw_reset();
-	printf("finished. releasing resources ...\n");
-	release_map_locks(map_locks);
-	free(vehicle_info);
-	free(blinkers);
-	printf("good bye.\n");
-#endif
+    // 	/* dealloc */
+    // 	map_draw_reset();
+    // 	printf("finished. releasing resources ...\n");
+    // 	release_map_locks(map_locks);
+    // 	free(vehicle_info);
+    // 	free(blinkers);
+    // 	printf("good bye.\n");
+    // #endif
 }
--- projects/crossroads/vehicle.c
+++ projects/crossroads/vehicle.c
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <string.h>
 
 #include "threads/thread.h"
 #include "threads/synch.h"
@@ -9,138 +10,360 @@
 
 /* path. A:0 B:1 C:2 D:3 */
 const struct position vehicle_path[4][4][12] = {
-	/* from A */ {
-		/* to A */
-		{{4,0},{4,1},{4,2},{4,3},{4,4},{3,4},{2,4},{2,3},{2,2},{2,1},{2,0},{-1,-1},},
-		/* to B */
-		{{4,0},{4,1},{4,2},{5,2},{6,2},{-1,-1},},
-		/* to C */
-		{{4,0},{4,1},{4,2},{4,3},{4,4},{4,5},{4,6},{-1,-1},},
-		/* to D */
-		{{4,0},{4,1},{4,2},{4,3},{4,4},{3,4},{2,4},{1,4},{0,4},{-1,-1},}
-	},
-	/* from B */ {
-		/* to A */
-		{{6,4},{5,4},{4,4},{3,4},{2,4},{2,3},{2,2},{2,1},{2,0},{-1,-1},},
-		/* to B */
-		{{6,4},{5,4},{4,4},{3,4},{2,4},{2,3},{2,2},{3,2},{4,2},{5,2},{6,2},{-1,-1},},
-		/* to C */
-		{{6,4},{5,4},{4,4},{4,5},{4,6},{-1,-1},},
-		/* to D */
-		{{6,4},{5,4},{4,4},{3,4},{2,4},{1,4},{0,4},{-1,-1},}
-	},
-	/* from C */ {
-		/* to A */
-		{{2,6},{2,5},{2,4},{2,3},{2,2},{2,1},{2,0},{-1,-1},},
-		/* to B */
-		{{2,6},{2,5},{2,4},{2,3},{2,2},{3,2},{4,2},{5,2},{6,2},{-1,-1},},
-		/* to C */
-		{{2,6},{2,5},{2,4},{2,3},{2,2},{3,2},{4,2},{4,3},{4,4},{4,5},{4,6},{-1,-1},},
-		/* to D */
-		{{2,6},{2,5},{2,4},{1,4},{0,4},{-1,-1},}
-	},
-	/* from D */ {
-		/* to A */
-		{{0,2},{1,2},{2,2},{2,1},{2,0},{-1,-1},},
-		/* to B */
-		{{0,2},{1,2},{2,2},{3,2},{4,2},{5,2},{6,2},{-1,-1},},
-		/* to C */
-		{{0,2},{1,2},{2,2},{3,2},{4,2},{4,3},{4,4},{4,5},{4,6},{-1,-1},},
-		/* to D */
-		{{0,2},{1,2},{2,2},{3,2},{4,2},{4,3},{4,4},{3,4},{2,4},{1,4},{0,4},{-1,-1},}
-	}
-};
-
+    /* from A */ {
+        /* to A */
+        {
+            {4, 0},
+            {4, 1},
+            {4, 2},
+            {4, 3},
+            {4, 4},
+            {3, 4},
+            {2, 4},
+            {2, 3},
+            {2, 2},
+            {2, 1},
+            {2, 0},
+            {-1, -1},
+        },
+        /* to B */
+        {
+            {4, 0},
+            {4, 1},
+            {4, 2},
+            {5, 2},
+            {6, 2},
+            {-1, -1},
+        },
+        /* to C */
+        {
+            {4, 0},
+            {4, 1},
+            {4, 2},
+            {4, 3},
+            {4, 4},
+            {4, 5},
+            {4, 6},
+            {-1, -1},
+        },
+        /* to D */
+        {
+            {4, 0},
+            {4, 1},
+            {4, 2},
+            {4, 3},
+            {4, 4},
+            {3, 4},
+            {2, 4},
+            {1, 4},
+            {0, 4},
+            {-1, -1},
+        }},
+    /* from B */ {/* to A */
+                  {
+                      {6, 4},
+                      {5, 4},
+                      {4, 4},
+                      {3, 4},
+                      {2, 4},
+                      {2, 3},
+                      {2, 2},
+                      {2, 1},
+                      {2, 0},
+                      {-1, -1},
+                  },
+                  /* to B */
+                  {
+                      {6, 4},
+                      {5, 4},
+                      {4, 4},
+                      {3, 4},
+                      {2, 4},
+                      {2, 3},
+                      {2, 2},
+                      {3, 2},
+                      {4, 2},
+                      {5, 2},
+                      {6, 2},
+                      {-1, -1},
+                  },
+                  /* to C */
+                  {
+                      {6, 4},
+                      {5, 4},
+                      {4, 4},
+                      {4, 5},
+                      {4, 6},
+                      {-1, -1},
+                  },
+                  /* to D */
+                  {
+                      {6, 4},
+                      {5, 4},
+                      {4, 4},
+                      {3, 4},
+                      {2, 4},
+                      {1, 4},
+                      {0, 4},
+                      {-1, -1},
+                  }},
+    /* from C */ {/* to A */
+                  {
+                      {2, 6},
+                      {2, 5},
+                      {2, 4},
+                      {2, 3},
+                      {2, 2},
+                      {2, 1},
+                      {2, 0},
+                      {-1, -1},
+                  },
+                  /* to B */
+                  {
+                      {2, 6},
+                      {2, 5},
+                      {2, 4},
+                      {2, 3},
+                      {2, 2},
+                      {3, 2},
+                      {4, 2},
+                      {5, 2},
+                      {6, 2},
+                      {-1, -1},
+                  },
+                  /* to C */
+                  {
+                      {2, 6},
+                      {2, 5},
+                      {2, 4},
+                      {2, 3},
+                      {2, 2},
+                      {3, 2},
+                      {4, 2},
+                      {4, 3},
+                      {4, 4},
+                      {4, 5},
+                      {4, 6},
+                      {-1, -1},
+                  },
+                  /* to D */
+                  {
+                      {2, 6},
+                      {2, 5},
+                      {2, 4},
+                      {1, 4},
+                      {0, 4},
+                      {-1, -1},
+                  }},
+    /* from D */ {/* to A */
+                  {
+                      {0, 2},
+                      {1, 2},
+                      {2, 2},
+                      {2, 1},
+                      {2, 0},
+                      {-1, -1},
+                  },
+                  /* to B */
+                  {
+                      {0, 2},
+                      {1, 2},
+                      {2, 2},
+                      {3, 2},
+                      {4, 2},
+                      {5, 2},
+                      {6, 2},
+                      {-1, -1},
+                  },
+                  /* to C */
+                  {
+                      {0, 2},
+                      {1, 2},
+                      {2, 2},
+                      {3, 2},
+                      {4, 2},
+                      {4, 3},
+                      {4, 4},
+                      {4, 5},
+                      {4, 6},
+                      {-1, -1},
+                  },
+                  /* to D */
+                  {
+                      {0, 2},
+                      {1, 2},
+                      {2, 2},
+                      {3, 2},
+                      {4, 2},
+                      {4, 3},
+                      {4, 4},
+                      {3, 4},
+                      {2, 4},
+                      {1, 4},
+                      {0, 4},
+                      {-1, -1},
+                  }}};
 
 void parse_vehicles(struct vehicle_info *vehicle_info, char *input)
 {
-	for (int idx = 0; idx < 4; idx++){
-		vehicle_info[idx].id = 'a';
-		// initial state of vehicle must be VEHICLE_STATUS_READY
-		vehicle_info[idx].state = VEHICLE_STATUS_READY; 
-		vehicle_info[idx].start = 'A';
-		vehicle_info[idx].dest = 'A';
-		// type of vehlcle can be normal or ambulance
-		vehicle_info[idx].type = VEHICL_TYPE_NORMAL;
-		// if the vehicle is normal, set the arrival and golden_time -1
-		vehicle_info[idx].arrival = -1;
-		vehicle_info[idx].golden_time = -1;
-	}
+    int thread_cnt = 1;
+    for (int i = 0; (size_t)i < strlen(input); i++)
+    {
+        if (input[i] == ':')
+        {
+            thread_cnt++;
+        }
+    }
+
+    // 2) strtok_r로 ':'을 구분자로 사용하여 토큰 분리
+    char *token;
+    char *save_ptr = NULL;
+
+    token = strtok_r(input, ":", &save_ptr);
+    for (int idx = 0; idx < thread_cnt; idx++)
+    {
+        // 토큰이 NULL일 경우(잘못된 입력) erro출력 및 시스템 종료
+        if (token == NULL)
+        {
+            printf("Error: Not enough vehicle data provided.\n");
+            shutdown_power_off();
+        }
+        size_t len = strlen(token);
+
+        // 공통 필드: id, start, dest, state
+        // token[0] = id, token[1] = start, token[2] = dest
+        vehicle_info[idx].id = token[0];
+        vehicle_info[idx].start = token[1];
+        vehicle_info[idx].dest = token[2];
+        vehicle_info[idx].state = VEHICLE_STATUS_READY; // 상태는 항상 READY로 고정
+
+        if (len == 3)
+        {
+            // 길이가 3인 경우: 일반 차량
+            vehicle_info[idx].type = VEHICL_TYPE_NORMAL;
+            vehicle_info[idx].arrival = 0;
+            vehicle_info[idx].golden_time = 0;
+        }
+        else
+        {
+            // 길이가 3보다 큰 경우: 구문 예) "fAB5.12"
+            // type은 구급차(AMBULANCE)
+            vehicle_info[idx].type = VEHICL_TYPE_AMBULANCE;
+
+            // token + 3 위치부터 "arrival.golden_time" 형태
+            char *numbers = token + 3;
+            char *dot_pos = strchr(numbers, '.');
+            if (dot_pos != NULL)
+            {
+                // arrival 문자열의 길이
+                size_t arr_len = (size_t)(dot_pos - numbers);
+
+                // arrival 부분
+                char arrival_str[16] = {0};
+                strlcpy(arrival_str, numbers, arr_len + 1);
+
+                // golden_time 부분
+                char *golden_str = dot_pos + 1;
+
+                // 문자열을 정수로 변환 후 저장
+                vehicle_info[idx].arrival = atoi(arrival_str);
+                vehicle_info[idx].golden_time = atoi(golden_str);
+            }
+            else
+            {
+                // 혹시 '.'이 없을 경우 오류 출력 및 시스템 종료
+                printf("Error: ambulance must have golden time.\n");
+                shutdown_power_off();
+            }
+        }
+
+        // 다음 토큰으로 이동
+        token = strtok_r(NULL, ":", &save_ptr);
+    }
 }
 
 static int is_position_outside(struct position pos)
 {
-	return (pos.row == -1 || pos.col == -1);
+    return (pos.row == -1 || pos.col == -1);
 }
 
 /* return 0:termination, 1:success, -1:fail */
 static int try_move(int start, int dest, int step, struct vehicle_info *vi)
 {
-	struct position pos_cur, pos_next;
+    struct position pos_cur, pos_next;
 
-	pos_next = vehicle_path[start][dest][step];
-	pos_cur = vi->position;
+    pos_next = vehicle_path[start][dest][step];
+    pos_cur = vi->position;
 
-	if (vi->state == VEHICLE_STATUS_RUNNING) {
-		/* check termination */
-		if (is_position_outside(pos_next)) {
-			/* actual move */
-			vi->position.row = vi->position.col = -1;
-			/* release previous */
-			lock_release(&vi->map_locks[pos_cur.row][pos_cur.col]);
-			return 0;
-		}
-	}
+    if (vi->state == VEHICLE_STATUS_RUNNING)
+    {
+        /* check termination */
+        if (is_position_outside(pos_next))
+        {
+            /* actual move */
+            vi->position.row = vi->position.col = -1;
+            /* release previous */
+            lock_release(&vi->map_locks[pos_cur.row][pos_cur.col]);
+            return 0;
+        }
+    }
 
-	/* lock next position */
-	lock_acquire(&vi->map_locks[pos_next.row][pos_next.col]);
-	if (vi->state == VEHICLE_STATUS_READY) {
-		/* start this vehicle */
-		vi->state = VEHICLE_STATUS_RUNNING;
-	} else {
-		/* release current position */
-		lock_release(&vi->map_locks[pos_cur.row][pos_cur.col]);
-	}
-	/* update position */
-	vi->position = pos_next;
-	
-	return 1;
+    /* lock next position */
+    lock_acquire(&vi->map_locks[pos_next.row][pos_next.col]);
+    if (vi->state == VEHICLE_STATUS_READY)
+    {
+        /* start this vehicle */
+        vi->state = VEHICLE_STATUS_RUNNING;
+    }
+    else
+    {
+        /* release current position */
+        lock_release(&vi->map_locks[pos_cur.row][pos_cur.col]);
+    }
+    /* update position */
+    vi->position = pos_next;
+
+    return 1;
 }
 
-void init_on_mainthread(int thread_cnt){
-	/* Called once before spawning threads */
+void init_on_mainthread(int thread_cnt)
+{
+    /* Called once before spawning threads */
 }
 
 void vehicle_loop(void *_vi)
 {
-	int res;
-	int start, dest, step;
+    int res;
+    int start, dest, step;
 
-	struct vehicle_info *vi = _vi;
+    struct vehicle_info *vi = _vi;
 
-	start = vi->start - 'A';
-	dest = vi->dest - 'A';
+    start = vi->start - 'A';
+    dest = vi->dest - 'A';
 
-	vi->position.row = vi->position.col = -1;
-	vi->state = VEHICLE_STATUS_READY;
+    vi->position.row = vi->position.col = -1;
+    vi->state = VEHICLE_STATUS_READY;
 
-	step = 0;
-	while (1) {
-		/* vehicle main code */
-		res = try_move(start, dest, step, vi);
-		if (res == 1) {
-			step++;
-		}
+    step = 0;
+    while (1)
+    {
+        /* vehicle main code */
+        res = try_move(start, dest, step, vi);
+        if (res == 1)
+        {
+            step++;
+        }
 
-		/* termination condition. */ 
-		if (res == 0) {
-			break;
-		}
+        /* termination condition. */
+        if (res == 0)
+        {
+            break;
+        }
 
-		/* unitstep change! */
-		unitstep_changed();
-	}	
+        /* unitstep change! */
+        unitstep_changed();
+    }
 
-	/* status transition must happen before sema_up */
-	vi->state = VEHICLE_STATUS_FINISHED;
+    /* status transition must happen before sema_up */
+    vi->state = VEHICLE_STATUS_FINISHED;
 }
